18cd96214cd518919c72b71647e19fd93a88fc3a
Jeremy Shaw <jeremy@n-heptane.com>
2015-04-08 11:36:44 -0500

    added .nix files

---
+++ shell.nix
@@ -0,0 +1,20 @@
+with (import <nixpkgs> {}).pkgs;
+let pkg = haskellngPackages.callPackage
+            ({ mkDerivation, acid-state, base, clckwrks, haskeline, mtl
+             , network, parsec, stdenv
+             }:
+             mkDerivation {
+               pname = "clckwrks-cli";
+               version = "0.2.14";
+               src = ./.;
+               isLibrary = false;
+               isExecutable = true;
+               buildDepends = [
+                 acid-state base clckwrks haskeline mtl network parsec
+               ];
+               homepage = "http://www.clckwrks.com/";
+               description = "a command-line interface for adminstrating some aspects of clckwrks";
+               license = stdenv.lib.licenses.bsd3;
+             }) {};
+in
+  pkg.env
